82808d0a93adaba8149e114cb206f585da408f9c
Dar Levy <darlevy8@gmail.com>
2023-08-11 23:57:25 +0300

    Add logo icon to assets and inject it to navbar

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <HStack>
+      <Image src={logo} />
+    </HStack>
+  );
 };
 
 export default NavBar;
