bb982770191cf622ef962aa831538f90a913ae4b
Dar Levy <darlevy8@gmail.com>
2023-08-11 23:43:37 +0300

    Create Grid and items

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return;
+  return (
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
