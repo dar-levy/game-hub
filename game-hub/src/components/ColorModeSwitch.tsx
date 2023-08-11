d38651526711384b2b2851da11d8d29502d448c5
Dar Levy <darlevy8@gmail.com>
2023-08-12 00:20:47 +0300

    Inject switch into navbar and style a little

--- game-hub/src/components/ColorModeSwitch.tsx
+++ game-hub/src/components/ColorModeSwitch.tsx
@@ -1,9 +1,15 @@
-import { HStack, Switch, Text } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <HStack>
-      <Switch />
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
       <Text>Dark Mode</Text>
     </HStack>
   );
