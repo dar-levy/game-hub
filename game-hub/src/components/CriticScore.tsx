2af5beae82d022139f93ed5f711bea17d91c284f
Dar Levy <darlevy8@gmail.com>
2023-08-12 18:05:55 +0300

    Style score badge

--- game-hub/src/components/CriticScore.tsx
+++ game-hub/src/components/CriticScore.tsx
@@ -5,7 +5,13 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  return <Badge>{score}</Badge>;
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
 };
 
 export default CriticScore;
