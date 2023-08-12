8cd73807e1c2cd4494392ca2279369257f6cb449
Dar Levy <darlevy8@gmail.com>
2023-08-12 22:09:28 +0300

    Get platforms from rawg.io

--- game-hub/src/components/PlatformSelector.tsx
+++ game-hub/src/components/PlatformSelector.tsx
@@ -1,17 +1,23 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/all";
-import React from "react";
+import React, { useState } from "react";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
 const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         Platforms
       </MenuButton>
       <MenuList>
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
+        {data.map((platform) => (
+          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
