2a8d48642395e7495b1e9f19fe2dda6d209c4e23
Dar Levy <darlevy8@gmail.com>
2023-08-12 23:33:12 +0300

    Create dummy functionality on search submit

--- game-hub/src/components/SearchInput.tsx
+++ game-hub/src/components/SearchInput.tsx
@@ -1,12 +1,26 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/all";
+import React, { useRef } from "react";
 
 const SearchInput = () => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) console.log(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
