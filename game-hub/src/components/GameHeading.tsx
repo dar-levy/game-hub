529d6f9f91aa322c2493b2ee2ffd073347ce47e8
Dar Levy <darlevy8@gmail.com>
2023-08-12 23:48:58 +0300

    Set heading as template literal

--- game-hub/src/components/GameHeading.tsx
+++ game-hub/src/components/GameHeading.tsx
@@ -1,7 +1,14 @@
 import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
 
-const GameHeading = () => {
-  return <Heading as="h1"></Heading>;
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name} ${gameQuery.genre?.name} Games`;
+
+  return <Heading as="h1">{heading}</Heading>;
 };
 
 export default GameHeading;
