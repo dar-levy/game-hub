4521d1d62721dd8fbe41a09c4d8c1408e0e57ed7
Dar Levy <darlevy8@gmail.com>
2023-08-12 19:39:55 +0300

    Display spinner while waiting to genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,10 +1,13 @@
 import useGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+
+  if (isLoading) return <Spinner />;
+
   return (
     <List>
       {data.map((genre) => (
