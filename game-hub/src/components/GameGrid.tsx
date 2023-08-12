3251a693698a1d065673181dfd42d40bff7067ad
Dar Levy <darlevy8@gmail.com>
2023-08-12 14:56:00 +0300

    Create GameGrid component's skeleton

---
+++ game-hub/src/components/GameGrid.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+const GameGrid = () => {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(() => {});
+  return <div>GameGrid</div>;
+};
+
+export default GameGrid;
