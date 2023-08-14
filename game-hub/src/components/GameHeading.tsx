5230216d1f23d9f4c8b527bf9eabb4cb4d934a86
Dar Levy <darlevy8@gmail.com>
2023-08-15 02:17:41 +0300

    Create usePlatform hook in order to remove duplicated code

--- game-hub/src/components/GameHeading.tsx
+++ game-hub/src/components/GameHeading.tsx
@@ -2,6 +2,7 @@ import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGenres from "../hooks/useGenres";
 import usePlatforms from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   gameQuery: GameQuery;
@@ -10,11 +11,7 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const { data: genres } = useGenres();
   const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
-
+  const platform = usePlatform(gameQuery.platformId);
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
 
   return (
