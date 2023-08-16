21061b58ef309368d153515265fbf9419b691867
Dar Levy <darlevy8@gmail.com>
2023-08-16 21:16:37 +0300

    Wrap items within SimpleGrid

--- game-hub/src/pages/GameDetailPage.tsx
+++ game-hub/src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import React from "react";
 import DefinitionItem from "../components/DefinitionItem";
@@ -18,24 +18,26 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <DefinitionItem term="Platforms">
-        {game.parent_platforms?.map(({ platform }) => (
-          <Text key={platform.id}>{platform.name}</Text>
-        ))}
-      </DefinitionItem>
-      <DefinitionItem term="Metascore">
-        <CriticScore score={game.metacritic} />
-      </DefinitionItem>
-      <DefinitionItem term="Genres">
-        {game.genres.map((genre) => (
-          <Text key={genre.id}>{genre.name}</Text>
-        ))}
-      </DefinitionItem>
-      <DefinitionItem term="Publishers">
-        {game.publishers?.map((publisher) => (
-          <Text key={publisher.id}>{publisher.name}</Text>
-        ))}
-      </DefinitionItem>
+      <SimpleGrid columns={2} as="dl">
+        <DefinitionItem term="Platforms">
+          {game.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Metascore">
+          <CriticScore score={game.metacritic} />
+        </DefinitionItem>
+        <DefinitionItem term="Genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Publishers">
+          {game.publishers?.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
+      </SimpleGrid>
     </>
   );
 };
