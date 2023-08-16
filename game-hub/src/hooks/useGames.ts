e5e6c4601a47a78f3e8cf9e20dcb866c8e12d8da
Dar Levy <darlevy8@gmail.com>
2023-08-16 14:33:52 +0300

    Link games by slug to GameDetailPage

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -8,7 +8,9 @@ const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
+
   name: string;
+  slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
